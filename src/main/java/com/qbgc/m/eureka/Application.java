79ca1632ac59c816736218cd8fff50bc659fef41
gaochao <gaochao5kuba@163,com>
2019-01-31 18:11:49 +0800

    修改为yml和删除不必要的文件

--- src/main/java/com/qbgc/m/eureka/Application.java
+++ src/main/java/com/qbgc/m/eureka/Application.java
@@ -2,12 +2,14 @@ package com.qbgc.m.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class QbgcMEurekaApplication {
+@EnableEurekaServer
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QbgcMEurekaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
