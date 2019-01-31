be672ecdc023fb552c27f767261d960bd069a8ae
gaochao <gaochao5kuba@163,com>
2019-01-31 13:50:09 +0800

    第一次上传服务注册中心

---
+++ src/main/java/com/qbgc/m/eureka/QbgcMEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.qbgc.m.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbgcMEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbgcMEurekaApplication.class, args);
+	}
+
+}
+
